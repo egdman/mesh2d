31782a9d4ba784fddae13d0c322c95d9ffe2acb9
egdman <egdman90@gmail.com>
2017-02-15 23:30:12 +0700

    added unique tag generation for draw objects

--- views/views.py
+++ views/views.py
@@ -1,5 +1,6 @@
 from mesh2d import Vector2, Matrix
 import random
+import uuid
 
 class ObjectView(object):
     '''
@@ -7,15 +8,11 @@ class ObjectView(object):
     This class is intended to be subclassed to draw concrete kinds of data such
     as points, lines, polygons, navmeshes, various helpers etc.
     '''
-    def __init__(self, tags=None):
+    def __init__(self):
         self.crd_buf = []
         self.element_ids = []
-
-        if tags is not None:
-            self.tags = tags
-        else:
-            self.tags = []
-
+        # generate unique tag for all canvas objects of this draw object
+        self.tag = uuid.uuid4().hex
 
 
     def first_time_draw(self, canvas):
@@ -37,8 +34,11 @@ class ObjectView(object):
         if len(self.crd_buf) == 0:
             self.first_time_draw(canvas)
 
-            # remember coordinate buffer
+            # add this tag to all canvas objects of this draw object
             for eid in self.element_ids:
+                canvas.addtag_withtag(self.tag, eid)
+
+                # remember all coordinates of all canvas objects of this draw object
                 self.crd_buf.extend(canvas.coords(eid))
 
             print("new object with {} coordinates created".format(len(self.crd_buf)))
@@ -62,9 +62,7 @@ class ObjectView(object):
 
 
     def cleanup(self, canvas):
-        for eid in self.element_ids:
-            canvas.delete(eid)
-
+        canvas.delete(self.tag)
 
 
 
@@ -79,15 +77,15 @@ class ObjectView(object):
 
         screen_vertices = list(self.apply_transform(obj_view, obj_vertices))
 
-        screen_vert_buf = []
+        screen_crd_buf = []
         for screen_v in screen_vertices:
-            screen_vert_buf.append(screen_v[0])
-            screen_vert_buf.append(screen_v[1])
+            screen_crd_buf.append(screen_v[0])
+            screen_crd_buf.append(screen_v[1])
 
         notch = 0
         for eid in self.element_ids:
             num_crds = len(canvas.coords(eid))
-            new_crds = screen_vert_buf[notch:notch+num_crds]
+            new_crds = screen_crd_buf[notch:notch+num_crds]
             canvas.coords(eid, *new_crds)
             notch += num_crds
 
@@ -197,10 +195,10 @@ class WallHelperView(ObjectView):
 
 
 class PointHelperView(ObjectView):
-    def __init__(self, loc, color='#ffffff', tags=None):
+    def __init__(self, loc, color='#ffffff'):
         self.loc = loc
         self.color = color
-        super(PointHelperView, self).__init__(tags)
+        super(PointHelperView, self).__init__()
 
 
     def modify(self, vec):
@@ -226,21 +224,20 @@ class PointHelperView(ObjectView):
                 self.loc[1] + sz,
             ),
 
-            fill = self.color,
-            tags = self.tags
+            fill = self.color
         )
         self.element_ids.append(Id)
 
 
 
 class SegmentHelperView(ObjectView):
-    def __init__(self, vert1, vert2, color='#ffffff', width=1, tags=None):
+    def __init__(self, vert1, vert2, color='#ffffff', width=1):
         self.v1 = vert1
         self.v2 = vert2
         self.width = width
 
         self.color = color
-        super(SegmentHelperView, self).__init__(tags)
+        super(SegmentHelperView, self).__init__()
 
 
 
@@ -251,8 +248,7 @@ class SegmentHelperView(ObjectView):
                     self.v2[0], self.v2[1]
                 ),
                 fill = self.color,
-                width=self.width,
-                tags = self.tags
+                width=self.width
         )
         self.element_ids.append(Id)
 
@@ -274,7 +270,7 @@ class SegmentHelperView(ObjectView):
 
 
 class PlusView(ObjectView):
-    def __init__(self, size, tags=None, loc=None, color='#2f2f2f'):
+    def __init__(self, size, loc=None, color='#2f2f2f'):
         self.size = size
         self.color = color
         self.loc = loc if loc is not None else Vector2(0, 0)
@@ -282,7 +278,7 @@ class PlusView(ObjectView):
         self.down = Vector2(0, self.size)
         self.right = Vector2(self.size, 0)
 
-        super(PlusView, self).__init__(tags)
+        super(PlusView, self).__init__()
 
 
 
@@ -300,9 +296,9 @@ class PlusView(ObjectView):
         # vrt = [Vector2(0, -sz),Vector2(0, sz),Vector2(-sz, 0),Vector2(sz, 0),]
         # trans_vrt = list(self.apply_transform(camera_transform, vrt))
         crds = self.get_open_crds(vrt, (0, 1))
-        id1 = canvas.create_line(crds, tags=self.tags, fill=self.color, width=1)
+        id1 = canvas.create_line(crds, fill=self.color, width=1)
         crds = self.get_open_crds(vrt, (2, 3))
-        id2 = canvas.create_line(crds, tags=self.tags, fill=self.color, width=1)
+        id2 = canvas.create_line(crds, fill=self.color, width=1)
         self.element_ids.append(id1)
         self.element_ids.append(id2)
 
@@ -326,13 +322,13 @@ class PolygonView(ObjectView):
 
 class NavMeshView(ObjectView):
 
-    def __init__(self, navmesh, tags=None):
+    def __init__(self, navmesh):
         rnd = lambda: random.randint(0,255)
         self.color = '#%02X%02X%02X' % (rnd(),rnd(),rnd())
         self.navmesh = navmesh
         self.obj_trans = Matrix.identity(3)
 
-        super(NavMeshView, self).__init__(tags)
+        super(NavMeshView, self).__init__()
 
 
 
@@ -343,16 +339,16 @@ class NavMeshView(ObjectView):
         # draw pieces
         for piece in self.navmesh.pieces:
             piece_crds = self.get_open_crds(trans_vertices, piece)
-            Id = canvas.create_polygon(piece_crds, tags=self.tags, fill='#1A1A1A', activefill='#111111')
+            Id = canvas.create_polygon(piece_crds, fill='#1A1A1A', activefill='#111111')
             self.element_ids.append(Id)
 
         # draw portals
         for portal in self.navmesh.portals:
             portal_crds = self.get_open_crds(trans_vertices, portal)
-            Id = canvas.create_line(portal_crds, tags=self.tags, fill='red', width=1)
+            Id = canvas.create_line(portal_crds, fill='red', width=1)
             self.element_ids.append(Id)
 
         # draw outline
         outline_crds = self.get_closed_crds(trans_vertices, self.navmesh.indices)
-        Id = canvas.create_line(outline_crds, tags=self.tags, fill='#FFFFFF', width=1)
+        Id = canvas.create_line(outline_crds, fill='#FFFFFF', width=1)
         self.element_ids.append(Id)
